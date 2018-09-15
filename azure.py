26f5ea7a04241fea0d283befa746f96a5a71ed82
jgoping <32006038+jgoping@users.noreply.github.com>
2018-09-15 01:48:25 -0400

    Initial commit

---
+++ azure.py
@@ -0,0 +1,37 @@
+import requests
+# If you are using a Jupyter notebook, uncomment the following line.
+#%matplotlib inline
+import matplotlib.pyplot as plt
+from PIL import Image
+from matplotlib import patches
+from io import BytesIO
+
+# Replace <Subscription Key> with your valid subscription key.
+subscription_key = "fc6112eed44a49c0aaf8b57c84109281"
+assert subscription_key
+
+# You must use the same region in your REST call as you used to get your
+# subscription keys. For example, if you got your subscription keys from
+# westus, replace "westcentralus" in the URI below with "westus".
+#
+# Free trial subscription keys are generated in the westcentralus region.
+# If you use a free trial subscription key, you shouldn't need to change
+# this region.
+face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
+
+# Set image_url to the URL of an image that you want to analyze.
+# image_url = 'https://how-old.net/Images/faces2/main007.jpg'
+image_url = 'https://img.buzzfeed.com/buzzfeed-static/static/2015-01/26/21/enhanced/webdr09/original-1014-1422325554-21.jpg?downsize=715:*&output-format=auto&output-quality=auto'
+# image_url = 'https://static1.squarespace.com/static/5784c5bc2994ca687a5cd9f9/59afcd0ff9a61eccbf986af8/59afcd0fa9db09ad7b9dd663/1504693984443/thomas-ruff-portraits.png?format=500w'
+
+headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+params = {
+    'returnFaceId': 'false',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': 'emotion'
+}
+data = {'url': image_url}
+response = requests.post(face_api_url, params=params, headers=headers, json=data)
+faces = response.json()
+
+print faces[0]["faceAttributes"]["emotion"]
